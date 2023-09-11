76f7af657ffef47f79371aec048b3e0a1d24b683
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2023-09-10 23:12:50 -0400

    added a list to remove and add books

--- Main.java
+++ Main.java
@@ -5,4 +5,7 @@ public class Main {
 
 public static void main(String[] args) {
     books List = new bookList();
+    books name = new bookName();
+    books add = new bookadd();
+    books remove = new bookremove();
 }
